099efd1135e380fa032bc508a590b4e67905db45
WARDKEY <tjrmsduq@naver.com>
2024-09-06 21:00:32 +0900

    단위 테스트 - Junit5로 테스트하기

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.unit.beverage;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmericanoTest {
+
+	@Test
+	void getName() {
+		Americano americano = new Americano();
+
+		// junit 사용
+		assertEquals(americano.getName(), "아메리카노");
+
+		// assertJ 사용
+		assertThat(americano.getName()).isEqualTo("아메리카노");
+	}
+
+	@Test
+	void getPrice() {
+		Americano americano = new Americano();
+
+		assertThat(americano.getPrice()).isEqualTo(4000);
+	}
+}
